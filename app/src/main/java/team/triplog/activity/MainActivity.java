0eab278dae45653c391a4e8d056d63c0d41af507
Minjae Kim <alswo5052@gmail.com>
2020-03-23 22:30:21 +0900

    MainActivity bottom navigation fragment 변경 연동

--- app/src/main/java/team/triplog/activity/MainActivity.java
+++ app/src/main/java/team/triplog/activity/MainActivity.java
@@ -1,16 +1,83 @@
 package team.triplog.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
+import team.triplog.MainHomeFragment;
+import team.triplog.MainMoreFragment;
+import team.triplog.MainSearchFragment;
+import team.triplog.MainTripFragment;
 import team.triplog.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout fragmentMain;
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        init();
+        setData();
+        setUi();
     }
+
+    private void init() {
+        fragmentMain = findViewById(R.id.fragment_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+    }
+
+    private void setData() {
+
+    }
+
+    private void setUi() {
+        fragmentChange(new MainHomeFragment());
+    }
+
+    private void fragmentChange(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_main, fragment);
+        fragmentTransaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    menuItem.setChecked(true);
+                    switch (menuItem.getItemId()) {
+                        case R.id.navigation_home:
+                            fragmentChange(new MainHomeFragment());
+                            break;
+                        case R.id.navigation_trip:
+                            fragmentChange(new MainTripFragment());
+                            break;
+                        case R.id.navigation_search:
+                            fragmentChange(new MainSearchFragment());
+                            break;
+                        case R.id.navigation_more:
+                            fragmentChange(new MainMoreFragment());
+                            break;
+                    }
+                    return false;
+                }
+            };
 }
